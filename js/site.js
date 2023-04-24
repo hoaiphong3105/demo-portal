523e9239b94d2e0a59a8855cb8f1eb9630e03e90
Dang Hoai Phong <phongdh10@fpt.com.vn>
2023-04-24 20:47:12 +0700

    init

---
+++ js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $('.menu-hover').hover(function () {
+        $('.menu-hover').stop().fadeTo('fast', 0.3);
+        $(this).stop().fadeTo('fast', 1);
+    }, function () {
+        $('.menu-hover').stop().fadeTo('fast', 1);
+    });
+});
